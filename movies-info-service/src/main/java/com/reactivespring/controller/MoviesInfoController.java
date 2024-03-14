4aff75265d6d37c4a65d78dce303b28b09bbc180
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-03-14 14:37:55 -0300

    Criação do controller de MovieInfo e sua respectiva classe de teste.

---
+++ movies-info-service/src/main/java/com/reactivespring/controller/MoviesInfoController.java
@@ -0,0 +1,42 @@
+package com.reactivespring.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.reactivespring.domain.MovieInfo;
+import com.reactivespring.service.MoviesInfoService;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/v1")
+public class MoviesInfoController {
+
+    private MoviesInfoService moviesInfoService;
+
+    public MoviesInfoController(MoviesInfoService moviesInfoService) {
+        this.moviesInfoService = moviesInfoService;
+    }
+
+    @GetMapping("/movieinfos")
+    public Flux<MovieInfo> getAllMovieInfos() {
+        
+        return moviesInfoService.getAllMovieInfos();
+    }
+
+    @PostMapping("/movieinfos")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<MovieInfo> addMovieInfo(@RequestBody MovieInfo movieInfo) {
+        
+        return moviesInfoService.addMovieInfo(movieInfo).log();
+    }
+    
+}
---
+++ movies-info-service/src/test/java/com/reactivespring/controller/MoviesInfoControllerIntgTest.java
@@ -0,0 +1,91 @@
+package com.reactivespring.controller;
+
+import java.net.URI;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import com.reactivespring.domain.MovieInfo;
+import com.reactivespring.repository.MovieInfoRepository;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@AutoConfigureWebTestClient
+public class MoviesInfoControllerIntgTest {
+    
+    private static final String MOVIES_INFO_URL = "/v1/movieinfos";
+
+    @Autowired
+    MovieInfoRepository movieInfoRepository;
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @BeforeEach
+    void setUp() {
+        
+        List<MovieInfo> movieinfos = Arrays.asList(new MovieInfo(null, "Batman Begins",
+                        2005, Arrays.asList("Christian Bale", "Michael Cane"), LocalDate.parse("2005-06-15")),
+                new MovieInfo(null, "The Dark Knight",
+                        2008, Arrays.asList("Christian Bale", "HeathLedger"), LocalDate.parse("2008-07-18")),
+                new MovieInfo("abc", "Dark Knight Rises",
+                        2012, Arrays.asList("Christian Bale", "Tom Hardy"), LocalDate.parse("2012-07-20")));
+        
+        movieInfoRepository.saveAll(movieinfos)
+            .blockLast();
+    }
+
+    @AfterEach
+    void tearDown() {
+        
+        movieInfoRepository.deleteAll().block();
+    }
+
+    @Test
+    void addMovieInfo() {
+
+        //given
+        MovieInfo movieInfo = new MovieInfo(null, "Batman Begins1",
+                        2005, Arrays.asList("Christian Bale", "Michael Cane"), LocalDate.parse("2005-06-15"));
+
+        //when
+        webTestClient
+            .post()
+            .uri(MOVIES_INFO_URL)
+            .bodyValue(movieInfo)
+            .exchange()
+            .expectStatus()
+            .isCreated()
+            .expectBody(MovieInfo.class)
+            .consumeWith(movieInfoEntityExchangeResult -> {
+
+                MovieInfo savedMovieInfo = movieInfoEntityExchangeResult.getResponseBody();
+                assert savedMovieInfo!=null;
+                assert savedMovieInfo.getMovieInfoId()!=null;
+            });
+
+        //then
+    }
+
+    @Test
+    void getAllMovieInfos() {
+        
+        webTestClient
+            .get()
+            .uri(MOVIES_INFO_URL)
+            .exchange()
+            .expectStatus()
+            .is2xxSuccessful()
+            .expectBodyList(MovieInfo.class)
+            .hasSize(3);
+    }
+}
