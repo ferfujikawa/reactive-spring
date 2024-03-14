dbb39ecd783095dcd178e65b04cce2c0f0d95604
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-03-14 14:37:34 -0300

    Criação da classe de serviço de MovieInfo.

---
+++ movies-info-service/src/main/java/com/reactivespring/service/MoviesInfoService.java
@@ -0,0 +1,30 @@
+package com.reactivespring.service;
+
+import org.springframework.stereotype.Service;
+
+import com.reactivespring.domain.MovieInfo;
+import com.reactivespring.repository.MovieInfoRepository;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class MoviesInfoService {
+
+    private MovieInfoRepository movieInfoRepository;
+    
+    public MoviesInfoService(MovieInfoRepository movieInfoRepository) {
+        this.movieInfoRepository = movieInfoRepository;
+    }
+
+    public Mono<MovieInfo> addMovieInfo(MovieInfo movieInfo) {
+        
+        return movieInfoRepository.save(movieInfo);
+    }
+
+    public Flux<MovieInfo> getAllMovieInfos() {
+        
+        return movieInfoRepository.findAll();
+    }
+
+}
