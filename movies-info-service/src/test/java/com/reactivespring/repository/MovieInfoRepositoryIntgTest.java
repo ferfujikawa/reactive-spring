4a583a93c49ed7ad6ec02435fc346ed7d231b977
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-03-25 15:36:19 -0300

    Adding test method to check the response of findByYear method of MovieInfoRepository.

--- movies-info-service/src/test/java/com/reactivespring/repository/MovieInfoRepositoryIntgTest.java
+++ movies-info-service/src/test/java/com/reactivespring/repository/MovieInfoRepositoryIntgTest.java
@@ -127,4 +127,18 @@ public class MovieInfoRepositoryIntgTest {
             .expectNextCount(2)
             .verifyComplete();
     }
+
+    @Test
+    void findByYear() {
+        
+        //given
+
+        //when
+        Flux<MovieInfo> moviesInfoFlux = movieInfoRepository.findByYear(2005).log();
+
+        //then
+        StepVerifier.create(moviesInfoFlux)
+            .expectNextCount(1)
+            .verifyComplete();
+    }
 }
