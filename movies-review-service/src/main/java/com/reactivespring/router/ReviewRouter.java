dd35bb62e65e54ad31c0b7579e78c96236c7274f
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-03-26 13:16:43 -0300

    Adding route to reviews POST request.

--- movies-review-service/src/main/java/com/reactivespring/router/ReviewRouter.java
+++ movies-review-service/src/main/java/com/reactivespring/router/ReviewRouter.java
@@ -4,16 +4,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
+
+import com.reactivespring.handler.ReviewHandler;
+
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
 public class ReviewRouter {
 
     @Bean
-    public RouterFunction<ServerResponse> reviewsRoute() {
+    public RouterFunction<ServerResponse> reviewsRoute(ReviewHandler reviewHandler) {
         
         return route()
             .GET("/v1/helloworld", (request -> ServerResponse.ok().bodyValue("helloworld")))
+            .POST("/v1/reviews", request -> reviewHandler.addReview(request))
             .build();
     }
 }
