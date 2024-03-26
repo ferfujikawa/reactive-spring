1cce3aad15a4338b5829f023e5de6cf40997e7eb
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-03-26 16:49:17 -0300

    Creting Movie, MovieInfo and Review classes.

---
+++ movies-service/src/main/java/com/reactivespring/domain/MovieInfo.java
@@ -0,0 +1,31 @@
+package com.reactivespring.domain;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+import org.springframework.validation.annotation.Validated;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Validated
+public class MovieInfo {
+
+    private String movieInfoId;
+    @NotBlank(message = "movieInfo.name must be present")
+    private String name;
+    @NotNull
+    @Positive(message = "movieInfo.year must be a Positive value")
+    private Integer year;
+    private List<@NotBlank(message = "movieInfo.cast must be present") String> cast;
+    private LocalDate release_date;
+}
+
