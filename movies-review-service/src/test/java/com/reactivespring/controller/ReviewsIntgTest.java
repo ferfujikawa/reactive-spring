1b254ab6d18507d397f166af043b82857d812db1
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-03-26 14:50:34 -0300

    Adding integration test methods to getAllReviews, updateReview, deleteReview and getReviewByMovieInfoId.

--- movies-review-service/src/test/java/com/reactivespring/controller/ReviewsIntgTest.java
+++ movies-review-service/src/test/java/com/reactivespring/controller/ReviewsIntgTest.java
@@ -1,5 +1,8 @@
 package com.reactivespring.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,6 +14,7 @@ import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWeb
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.reactivespring.domain.Review;
 import com.reactivespring.repository.ReviewReactiveRepository;
@@ -34,7 +38,7 @@ public class ReviewsIntgTest {
         List<Review> reviewsList = Arrays.asList(
             new Review(null, 1L, "Awesome Movie", 9.0),
             new Review(null, 1L, "Awesome Movie1", 9.0),
-            new Review(null, 2L, "Excellent Movie", 8.0));
+            new Review("123456", 2L, "Excellent Movie", 8.0));
         reviewReactiveRepository.saveAll(reviewsList)
             .blockLast();
     }
@@ -65,5 +69,78 @@ public class ReviewsIntgTest {
                 assert savedReview!= null;
                 assert savedReview.getReviewId() != null;
             });
+    }
+
+    @Test
+    void getAllReviews() {
+        
+        webTestClient
+            .get()
+            .uri(REVIEWS_URL)
+            .exchange()
+            .expectStatus()
+            .is2xxSuccessful()
+            .expectBodyList(Review.class)
+            .hasSize(3);
+    }
+
+    @Test
+    void updateReview() {
+
+        //given
+        String reviewId = "123456";
+        Review review = new Review(null, 1L, "Awsome Movie Updated", 8.8);
+
+        //when
+        webTestClient
+            .put()
+            .uri(REVIEWS_URL + "/{id}", reviewId)
+            .bodyValue(review)
+            .exchange()
+            .expectStatus()
+            .is2xxSuccessful()
+            .expectBody(Review.class)
+            .consumeWith(reviewEntityExchangeResult -> {
+
+                Review updatedReview = reviewEntityExchangeResult.getResponseBody();
+                assert updatedReview!=null;
+                assertEquals("Awsome Movie Updated", updatedReview.getComment());
+                assertEquals(8.8, updatedReview.getRating());
+            });
+
+        //then
+    }
+
+    @Test
+    void deleteReview() {
+
+        //given
+        String reviewId = "123456";
+
+        //when
+        webTestClient
+            .delete()
+            .uri(REVIEWS_URL + "/{id}", reviewId)
+            .exchange()
+            .expectStatus()
+            .isNoContent();
+    }
+
+    @Test
+    void getReviewsByMovieInfoId() {
+        
+        //given
+        URI uri = UriComponentsBuilder.fromUriString(REVIEWS_URL)
+            .queryParam("movieInfoId", 1L)
+            .buildAndExpand().toUri();
+
+        webTestClient
+            .get()
+            .uri(uri)
+            .exchange()
+            .expectStatus()
+            .is2xxSuccessful()
+            .expectBodyList(Review.class)
+            .hasSize(2);
     }
 }
