23789bcbd9a5e2f9ecc06d91832fd32f966f06e2
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-03-26 13:49:10 -0300

    Adding method to handle updateReview request.

--- movies-review-service/src/main/java/com/reactivespring/handler/ReviewHandler.java
+++ movies-review-service/src/main/java/com/reactivespring/handler/ReviewHandler.java
@@ -32,4 +32,22 @@ public class ReviewHandler {
         Flux<Review> reviewsFlux = reviewReactiveRepository.findAll();
         return ServerResponse.ok().body(reviewsFlux, Review.class);
     }
+
+    public Mono<ServerResponse> updateReview(ServerRequest request) {
+        
+        String reviewId = request.pathVariable("id");
+
+        Mono<Review> existingReview = reviewReactiveRepository.findById(reviewId);
+
+        return existingReview
+            .flatMap(review -> request.bodyToMono(Review.class)
+                .map(reqReview -> {
+                    review.setComment(reqReview.getComment());
+                    review.setRating(reqReview.getRating());
+                    return review;
+                })
+                .flatMap(reviewReactiveRepository::save)
+                .flatMap(savedReview -> ServerResponse.ok().bodyValue(savedReview))
+            );
+    }
 }
