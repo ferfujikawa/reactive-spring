46d8628f47ecaafb2238bab728e6d35d2b22665b
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-03-26 16:12:04 -0300

    Adding validation annotations on Review class properties.

--- movies-review-service/src/main/java/com/reactivespring/domain/Review.java
+++ movies-review-service/src/main/java/com/reactivespring/domain/Review.java
@@ -1,5 +1,8 @@
 package com.reactivespring.domain;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -15,7 +18,9 @@ public class Review {
 
     @Id
     private String reviewId;
+    @NotNull(message = "rating.movieInfoId : must not be null")
     private Long movieInfoId;
     private String comment;
+    @Min(value = 0L, message = "rating.negative : please pass a non-negative value")
     private Double rating;
 }
