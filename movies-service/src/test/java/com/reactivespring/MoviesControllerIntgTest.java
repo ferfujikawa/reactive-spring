a1498dbba9ea1ef94cbf299fb9c6fe5c7501639c
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-03-27 13:47:17 -0300

    Adding test method to 404 result in MoviesInfoService API and another test method to 404 result in MoviesReviewService API.

--- movies-service/src/test/java/com/reactivespring/MoviesControllerIntgTest.java
+++ movies-service/src/test/java/com/reactivespring/MoviesControllerIntgTest.java
@@ -65,4 +65,54 @@ public class MoviesControllerIntgTest {
             });
 
     }
+
+    @Test
+    void retrieveMovieById_404() {
+
+        //given
+        String movieId = "abc";
+
+        stubFor(get(urlEqualTo("/v1/movieinfos/" + movieId))
+            .willReturn(aResponse()
+                .withStatus(404)));
+        
+        //when
+        webTestClient
+            .get()
+            .uri("/v1/movies/{id}", movieId)
+            .exchange()
+            .expectStatus()
+            .is4xxClientError()
+            .expectBody(String.class)
+            .isEqualTo("There is no MovieInfo Available for the passed Id : abc");
+    }
+
+    @Test
+    void retrieveMovieById_reviews_404() {
+
+        //given
+        String movieId = "abc";
+
+        stubFor(get(urlEqualTo("/v1/movieinfos/" + movieId))
+            .willReturn(aResponse()
+                .withHeader("Content-Type", "application/json")
+                .withBodyFile("movieinfo.json")));
+        
+        stubFor(get(urlPathEqualTo("/v1/reviews"))
+            .willReturn(aResponse()
+                .withStatus(404)));
+
+        //when
+        webTestClient
+            .get()
+            .uri("/v1/movies/{id}", movieId)
+            .exchange()
+            .expectStatus().isOk()
+            .expectBody(Movie.class)
+            .consumeWith(movieEntityExchangeResult -> {
+                Movie movie = movieEntityExchangeResult.getResponseBody();
+                assert Objects.requireNonNull(movie).getReviewList().size() == 0;
+                assertEquals("Batman Begins", movie.getMovieInfo().getName());
+            });
+    }
 }
